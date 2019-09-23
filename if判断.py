98cc41020b516ca1e398976e46c35984ccb36abc
tomreddle <844626764@qq.com>
2019-09-23 22:34:43 +0800

    第一次提交代码

---
+++ if判断.py
@@ -0,0 +1,24 @@
+a = int(input('>>>'))
+if 70<= a <= 100:
+    if a>=90:
+        print('A')
+    elif a>=80:
+        print('B')
+    else:
+        print('C')
+elif 0<a<70:
+    if 0<a<60:
+        print('7E')
+    else:
+        print('D')
+else:
+    print('error')
+
+'''
+90--100
+80-90
+70-80
+60-70
+--60
+
+'''
