fb21a16710512472e572a0b3291a328d12b7d71d
tomreddle <844626764@qq.com>
2019-10-16 23:02:51 +0800

    修改

--- mymodule.py
+++ mymodule.py
@@ -1,11 +1,14 @@
 def fun1():
     print('fun1')
 
+
 def fun2():
     print('fun2')
 
-print('文件的属性：',__name__)
 
-if __name__ =='__main__':
+print('文件的属性：', __name__)
+
+if __name__ == '__main__':
     fun1()
     fun2()
+
